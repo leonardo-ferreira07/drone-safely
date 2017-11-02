b14cd43c2bbac7d5c0fc1ef5024b440de185e6ec
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-11-02 18:52:56 +0800

    Created base project structure

---
+++ Drone Safely/Drone Safely/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drone Safely
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 02/11/17.
+//  Copyright © 2017 Leonardo Vinicius Kaminski Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
