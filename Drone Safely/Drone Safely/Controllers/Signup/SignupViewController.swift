18be3ea7e6e67867f213f5b0b1a36f2ab4cf53ac
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-11-03 18:34:18 +0800

    Added signup option

--- Drone Safely/Drone Safely/Controllers/Signup/SignupViewController.swift
+++ Drone Safely/Drone Safely/Controllers/Signup/SignupViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import Firebase
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIBarButtonItem!
+    
+    let reachability = Reachability()!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        observeChanges()
+        enableSignUpButton(false)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        resignTextFields()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -20,20 +35,66 @@ class SignupViewController: UIViewController {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Actions
+    
+    @IBAction func signUpButtonPressed(_ sender: UIBarButtonItem) {
+        resignTextFields()
+        view.startLoadingAnimation()
+        
+        guard reachability.connection != .none else {
+            view.stopLoadingAnimation()
+            showAlert("Connection Error", message: "Seems that you don't have internet connection.")
+            return
+        }
+        if let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text, email.isValidEmail(), !password.isEmpty, !name.isEmpty {
+            Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                self.view.stopLoadingAnimation()
+                if let error = error {
+                    print(error)
+                    return
+                }
+                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                changeRequest?.displayName = name
+                changeRequest?.commitChanges { (error) in
+                    if let error = error {
+                        print(error)
+                        return
+                    }
+                    
+                    self.performSegue(withIdentifier: "goMainStoryboard", sender: nil)
+                }
+            }
+        } else {
+            self.view.stopLoadingAnimation()
+            showAlert("Login Error", message: "Email or password is not in the correct format.")
+        }
     }
     
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - TextFields configs
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension SignupViewController: UITextFieldDelegate {
+    func resignTextFields() {
+        nameTextField.resignFirstResponder()
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
     }
-    */
-
+    
+    func observeChanges() {
+        nameTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        emailTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if !(nameTextField.text?.isEmpty ?? false) && !(emailTextField.text?.isEmpty ?? false) && !(passwordTextField.text?.isEmpty ?? false) {
+            enableSignUpButton(true)
+        }
+    }
+    
+    func enableSignUpButton(_ enabled: Bool) {
+        signUpButton.isEnabled = enabled
+    }
+    
 }
